c00716c954dbf23097ffae66e666cdf77dd90999
suzyng83209 <suzyn.one@gmail.com>
2017-12-08 10:47:41 -0500

    add .eslintrc and trailing commas

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ router.pose('/upload', (req, res, next) => {
     }
 
     S3Utils.upload(data);
-})
+});
 
 router.post('/merge', (req, res, next) => {
     var { data } = req.body;
@@ -24,26 +24,31 @@ router.post('/merge', (req, res, next) => {
         var _local, _remote, _combined;
         return Promise.all([
             Utils.downloadB64Data(local, `local-${Date.now()}.webm`),
-            Utils.downloadB64Data(remote, `remote-${Date.now()}.webm`)
-        ]).then(([localPath, remotePath]) => {
-            _local = localPath;
-            _remote = remotePath;
-            console.log('***PATHS***: ', localPath, remotePath);
-            var promise = remotePath ? ffmpegUtils.merge(localPath, remotePath) : Promise.resolve();
-            return promise;
-            // TODO: 2. GENERATE TRANSCRIPT
-        }).then(combinedPath => {
-            _combined = combinedPath;
-            
-            return Promise.map([
-                S3Utils.upload(_local),
-                S3Utils.upload(_remote),
-                S3Utils.upload(_combined)
-            ])
-            // TODO: 3. UPLOAD TO S3 || Google Cloud Storage
-        }).then(s3Key => {
-            return s3Key;
-        })
+            Utils.downloadB64Data(remote, `remote-${Date.now()}.webm`),
+        ])
+            .then(([localPath, remotePath]) => {
+                _local = localPath;
+                _remote = remotePath;
+                console.log('***PATHS***: ', localPath, remotePath);
+                var promise = remotePath
+                    ? ffmpegUtils.merge(localPath, remotePath)
+                    : Promise.resolve();
+                return promise;
+                // TODO: 2. GENERATE TRANSCRIPT
+            })
+            .then(combinedPath => {
+                _combined = combinedPath;
+
+                return Promise.map([
+                    S3Utils.upload(_local),
+                    S3Utils.upload(_remote),
+                    S3Utils.upload(_combined),
+                ]);
+                // TODO: 3. UPLOAD TO S3 || Google Cloud Storage
+            })
+            .then(s3Key => {
+                return s3Key;
+            });
     })
         .then(() => res.status(200).send({ success: true }))
         .catch(next);
